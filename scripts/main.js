c08509cfbd146789d828297838e65020f1258aad
JosephV95 <viltejl@gmail.com>
2023-02-24 12:59:46 -0300

    Se dinamizo las Cards de Home y pastEvents con Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,30 @@
+//? Capturamos un nodo del DOM (en el HTML).  
+const contenedor = document.getElementById("contenedor");
 
-// const contenedor = document.getElementById("contenedor");
+let tarjetas = "";
 
-// let tarjetas = "";
+function agregarCards(dataArray){
+    for (let persona of dataArray) {
+        tarjetas += `
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+            <div class="card " style="width: 20rem; height: 25rem; ">
+                <img src="${persona.image}" class="card-img-top" alt="...">
+                <div class="card-body text-center" style="position: relative";>
+                    <h4 class="card-title">${persona.name}</h4>
+                    <p class="card-text">${persona.description}</p>
+                    <a href="details.html" class="btn btn-primary" style="position: absolute; bottom:1rem; margin-left: -2rem;" >Details</a>
+                </div>
+            </div>
+        </div> `
+    }
+    return tarjetas;
+}
 
-// function agregarCards(dataArray){
-//     for (let persona of dataArray) {
-//         tarjetas += `
-//         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
-//             <div class="card" style="width: 20rem; height: 25rem;">
-//                 <img src="${persona.image}" class="card-img-top" alt="...">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${persona.name}</h5>
-//                     <p class="card-text">${persona.description}</p>
-//                     <a href="details.html" class="btn btn-primary">Details</a>
-//                 </div>
-//             </div>
-//         </div>`
-//     }
-//     return tarjetas;
-// }
+//? con innerHTML se le asigna texto html( con innerText solo se le asignaria strings)
+contenedor.innerHTML = agregarCards(data.events);
 
-// contenedor.innerHTML = agregarCards(data.events);
+// let fecha1 = new Date("2022-01-01");
+// let fecha2 = new Date("2022-04-15");
+
+// let fech = fecha1 < fecha2;
+// console.log(fecha1 < fecha2);
---
+++ scripts/past_ev.js
@@ -0,0 +1,31 @@
+
+//? Capturo el nodo del DOM(en html) a donde se le asignaran las cards
+const containerPast = document.getElementById("contenedorPast");
+
+//! fecha a ser comparada con las demas
+const fechaBase = new Date(data.currentDate); 
+
+let cards = "";
+
+function agregarCards(unArray){
+    for (let persona of unArray) {
+        if (new Date(persona.date) < fechaBase) {
+            cards += ` 
+            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+                <div class="card" style="width: 20rem; height: 25rem; ">
+                    <img src="${persona.image}" class="card-img-top" alt="...">
+                    <div class="card-body  text-center">
+                        <h4 class="card-title">${persona.name}</h4>
+                        <p class="card-text">${persona.description}</p>
+                        <a href="details.html" class="btn btn-primary"  style="position: absolute; bottom:1rem; margin-left: -2rem;">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div> `
+        }
+    }
+    return cards;
+}
+
+//?  Le asignamos el valor al template(html)
+containerPast.innerHTML = agregarCards(data.events); 
