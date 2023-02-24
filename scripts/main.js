5c3c3fd00466b70dc909d00b708fef661da3b272
JosephV95 <viltejl@gmail.com>
2023-02-24 00:52:35 -0300

    se arreglo el footer

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+// const contenedor = document.getElementById("contenedor");
+
+// let tarjetas = "";
+
+// function agregarCards(dataArray){
+//     for (let persona of dataArray) {
+//         tarjetas += `
+//         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
+//             <div class="card" style="width: 20rem; height: 25rem;">
+//                 <img src="${persona.image}" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${persona.name}</h5>
+//                     <p class="card-text">${persona.description}</p>
+//                     <a href="details.html" class="btn btn-primary">Details</a>
+//                 </div>
+//             </div>
+//         </div>`
+//     }
+//     return tarjetas;
+// }
+
+// contenedor.innerHTML = agregarCards(data.events);
