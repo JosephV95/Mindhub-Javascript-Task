c60017212e8768d1a30541103a3e52b9c10e9cb7
JosephV95 <viltejl@gmail.com>
2023-03-14 19:50:16 -0300

    Se agrego la carga del evento desde una API en Details

--- scripts/details.js
+++ scripts/details.js
@@ -5,29 +5,43 @@ const param = new URLSearchParams(location.search)
 
 const id = param.get("id")
 
-let cardDetail = data.events.find((event) => event._id == id);
+//! Se cargara las cards desde una API (con asincronismo)
+function traerDatosApi(){
+    // fetch('scripts/amazing_1.json')
+    fetch('https://mindhub-xj03.onrender.com/api/amazing')
+    .then(response => response.json())
+    .then(datosApi => {
 
-divDetail.innerHTML = `
-<div class="card border-primary bg-black text-white mb-3 mt-3 p-3" style="max-width: 100%; font-size: 1.2rem; font-family: 'Caveat', cursive;">
-  <div class="row g-3 ">
-    <div class="col-md-5 align-self-center">
-      <img src="${cardDetail.image}" class="align-self-center" style="width:100%; height: auto; " alt="img${cardDetail._id}">
-    </div>
-    <div class="col-md-7">
-      <div class="card-body">
-        <h3 class="card-title "><b>${cardDetail.name}</b></h3>
-        <p class="card-text"><i>${cardDetail.description}</i></p>
-        <p class="card-text"><b><u>Date</u>:</b> ${cardDetail.date}</p>
-        <p class="card-text"><b><u>Price</u>:</b> $${cardDetail.price}</p>
-        <p class="card-text"><b><u>Capacity</u>:</b> ${cardDetail.capacity} people</p>
-        <p class="card-text"><b><u>Estimate</u>:</b> ${cardDetail.estimate} </p>
-        <p class="card-text"><b><u>Category</u>:</b> ${cardDetail.category}</p>
-        <p class="card-text"><b><u>Place</u>:</b> ${cardDetail.place}</p>
-        <a href="index.html" class="card-link">Back to Home</a>
-      </div>
-    </div>
-  </div>
-</div>`
+        let cardDetail = datosApi.events.find((event) => event._id == id);
+        // console.log(cardDetail);
+        cargarCardDetail(cardDetail);
+    })
+    .catch(error => console.log(error.messagge))
+}
+traerDatosApi()
 
+function cargarCardDetail(eventoDetail){
+  return divDetail.innerHTML = `
+        <div class="card border-primary bg-black text-white mb-3 mt-3 p-3" style="max-width: 100%; font-size: 1.2rem; font-family: 'Caveat', cursive;">
+          <div class="row g-3 ">
+            <div class="col-md-5 align-self-center">
+              <img src="${eventoDetail.image}" class="align-self-center" style="width:100%; height: auto; " alt="img${eventoDetail._id}">
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <h3 class="card-title "><b>${eventoDetail.name}</b></h3>
+                <p class="card-text"><i>${eventoDetail.description}</i></p>
+                <p class="card-text"><b><u>Date</u>:</b> ${eventoDetail.date}</p>
+                <p class="card-text"><b><u>Price</u>:</b> $${eventoDetail.price}</p>
+                <p class="card-text"><b><u>Capacity</u>:</b> ${eventoDetail.capacity} people</p>
+                <p class="card-text"><b><u>Estimate</u>:</b> ${eventoDetail.estimate} </p>
+                <p class="card-text"><b><u>Category</u>:</b> ${eventoDetail.category}</p>
+                <p class="card-text"><b><u>Place</u>:</b> ${eventoDetail.place}</p>
+                <a href="index.html" class="card-link">Back to Home</a>
+              </div>
+            </div>
+          </div>
+        </div>`
+}
 
 
